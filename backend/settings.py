e815daadb32d4b279ab350a19b0c76dd992447d9
Yitong Wu <44213937+WUY97@users.noreply.github.com>
2022-10-05 21:50:22 -0700

    new commit

--- backend/settings.py
+++ backend/settings.py
@@ -172,8 +172,8 @@ DJOSER = {
 import django_heroku
 django_heroku.settings(locals())
 
-import dj-database-url
-prod_db=dj-database-url.config(conn_max_age=500)
+import dj_database_url
+prod_db=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 try:
